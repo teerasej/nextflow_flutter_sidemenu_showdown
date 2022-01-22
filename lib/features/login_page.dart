36344b44d94cef880d65c34696714b81dd69f619
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-01-22 16:48:50 +0700

    simulate login complete

--- lib/features/login_page.dart
+++ lib/features/login_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:nextflow_flutter_sidemenu_showdown/nav_util.dart';
+import 'package:nextflow_flutter_sidemenu_showdown/providers/user_provider.dart';
 
 import '../main_drawer.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,15 +15,16 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Login Page'),
       ),
-      // body: Padding(
-      //   padding: EdgeInsets.all(10),
-      //   child: ElevatedButton(
-      //     child: Text('sign up'),
-      //     onPressed: () {
-      //       Navigator.pushNamed(context, 'register/comfirmation');
-      //     },
-      //   ),
-      // ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ElevatedButton(
+          child: Text('Log in'),
+          onPressed: () {
+            context.read<UserProvider>().login();
+            Navigator.pushNamed(context, NavUtil.auction);
+          },
+        ),
+      ),
     );
   }
 }
