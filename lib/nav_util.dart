766564052e3f86981888f5fdb5cfd7d26264cf54
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-01-22 16:16:52 +0700

    create utility class to provide map of routing

---
+++ lib/nav_util.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'features/auction_detail_page.dart';
+import 'features/auction_page.dart';
+import 'features/home_page.dart';
+import 'features/register_confirm_page.dart';
+import 'features/register_page.dart';
+
+class NavUtil {
+  static String home = '/home';
+  static String login = '/login';
+  static String register = '/register';
+  static String registerConfirmation = '/register/confirmation';
+  static String auction = '/auction';
+  static String auctionDetail = '/auction/detail';
+
+  static Map<String, Widget Function(BuildContext)> routeMap = {
+    NavUtil.home: (context) => HomePage(),
+    NavUtil.register: (context) => RegisterPage(),
+    NavUtil.registerConfirmation: (context) => RegisterConfirmPage(),
+    NavUtil.auction: (context) => AuctionPage(),
+    NavUtil.auctionDetail: (context) => AuctionDetailPage(),
+  };
+}
