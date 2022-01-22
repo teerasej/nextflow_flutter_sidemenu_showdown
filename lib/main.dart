b56519dc9c1ffed856f64d34efe3ca1d95210c3b
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-01-22 16:48:37 +0700

    setup provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:nextflow_flutter_sidemenu_showdown/features/home_page.dart';
 import 'package:nextflow_flutter_sidemenu_showdown/nav_util.dart';
+import 'package:nextflow_flutter_sidemenu_showdown/providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => UserProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
