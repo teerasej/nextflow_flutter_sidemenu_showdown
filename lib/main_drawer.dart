2a659b5855f08f139e9dc9afe046da9a44d9db65
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-01-22 16:49:25 +0700

    make drawer's menu items show/hide related to user's login state

--- lib/main_drawer.dart
+++ lib/main_drawer.dart
@@ -1,32 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:nextflow_flutter_sidemenu_showdown/nav_util.dart';
+import 'package:nextflow_flutter_sidemenu_showdown/providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
 class MainDrawer extends StatelessWidget {
   const MainDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var _userProvider = context.read<UserProvider>();
+
     return Drawer(
       child: ListView(
         children: [
+          _userProvider.user != null
+              ? ListTile(
+                  title: Text('Hello, ${_userProvider.user?.username}'),
+                )
+              : Container(),
           ListTile(
             title: Text('Home'),
             onTap: () {
               Navigator.pushReplacementNamed(context, NavUtil.home);
             },
           ),
-          ListTile(
-            title: Text('Login'),
-            onTap: () {
-              Navigator.pushReplacementNamed(context, NavUtil.login);
-            },
-          ),
-          ListTile(
-            title: Text('Register'),
-            onTap: () {
-              Navigator.pushReplacementNamed(context, NavUtil.register);
-            },
-          ),
+          _userProvider.user == null
+              ? ListTile(
+                  title: Text('Login'),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, NavUtil.login);
+                  },
+                )
+              : Container(),
+          _userProvider.user == null
+              ? ListTile(
+                  title: Text('Register'),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, NavUtil.register);
+                  },
+                )
+              : Container(),
           ListTile(
             title: Text('Auction'),
             onTap: () {
